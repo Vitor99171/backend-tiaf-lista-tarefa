7aa0f73be953ce5f55d64baeb44ab71c20b85442
Vitor99171 <vitormanzolli99@gmail.com>
2024-10-17 09:45:45 -0300

    atualizações

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const cors = require('cors'); // Importa o CORS
 const tarefaRoutes = require('./routes/tarefaRoutes');
 const userRoutes = require('./routes/userRoutes');
 const authMiddleware = require('./middlewares/authMiddleware');
@@ -7,10 +8,14 @@ const authMiddleware = require('./middlewares/authMiddleware');
 // Middleware para JSON
 app.use(express.json());
 
+app.use(cors()); // Inicializa o CORS
+
 // Rotas
 app.use('/api', userRoutes); // Rota para autenticação
 
 // Rotas protegidas
 app.use('/api/tarefas', authMiddleware, tarefaRoutes);
+
+
 
 module.exports = app;
